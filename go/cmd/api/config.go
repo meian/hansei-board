5569201de98cfd740c87ae067cddd2fe76aa65ac
kitamin <11195207+meian@users.noreply.github.com>
2022-08-28 23:06:20 +0900

    ポートを可変にした

---
+++ go/cmd/api/config.go
@@ -0,0 +1,5 @@
+package main
+
+type AppConfig struct {
+	Port int `default:"3000"`
+}
--- go/cmd/api/main.go
+++ go/cmd/api/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/meian/hansei-board/go/core/template"
 )
 
+var (
+	PConv PreviewConfig
+)
+
 func main() {
+	envconfig.Process("APP", &AConf)
+	log.Printf("app config: %+v", AConf)
 	router := gin.Default()
 	router.GET("/templates", func(ctx *gin.Context) {
 		ctx.JSON(200, template.Names)
 	})
 	router.POST("/preview", Preview)
-	router.Run(":3000")
+	router.Run(fmt.Sprintf(":%d", AConf.Port))
 }
