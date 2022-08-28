f41581f5f48ea128b12d5e8ef42f2d4398361a0c
kitamin <11195207+meian@users.noreply.github.com>
2022-08-28 21:49:59 +0900

    JPEG quality=100で画像を作り直し

--- go/tools/normalize-img/normalize.sh
+++ go/tools/normalize-img/normalize.sh
@@ -2,8 +2,8 @@
 
 (
     cd "$(cd $(dirname $0); pwd)"
-    go run . -q 90 cat1
-    go run . -q 90 cat2
-    go run . -q 90 cat3
-    go run . -q 90 catp
+    go run . -q 100 cat1
+    go run . -q 100 cat2
+    go run . -q 100 cat3
+    go run . -q 100 catp
 )
